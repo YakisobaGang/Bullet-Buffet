9a7f423f40e6d2514a0cab6a7f044bfd4e8f0640
Silvanodja <71275880+Silvanodja@users.noreply.github.com>
2020-12-07 09:28:44 -0300

    07/10 1 commite

---
+++ Assets/PriceShop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class PriceShop : MonoBehaviour
+{
+    private TextMeshProUGUI texto;
+    public GameObject Referenza;
+    public int _referenza;
+
+    void Awake()
+    {
+        texto = GetComponent<TextMeshProUGUI>();
+        _referenza = Referenza.GetComponent<ShopSystem>()._actualValue;
+    }
+
+    void LateUpdate()
+    {
+        texto.SetText(_referenza.ToString());
+    }
+
+}
---
+++ Assets/ShopSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using YakisobaGang.Scripts;
+using Scripts;
+using YakisobaGang.Player.Scripts;
+
+public class ShopSystem : MonoBehaviour
+{
+    public int _value;
+    public float upgrade;
+    public int position;
+    private int _actualCash;
+    public int _actualValue;
+
+    private void Awake()
+    {
+        _value = _actualValue;
+    }
+    private void LateUpdate()
+    {
+        ScoreAndCashSystem.Cash = _actualCash;
+    }
+    public void Buy()
+    {
+
+        if (_actualCash > _actualValue)
+        {
+            ScoreAndCashSystem.Cash -= _value;
+            _actualValue += _value;
+            GunEdit(upgrade);
+        }
+
+    }
+    private void GunEdit(float fireRate)
+    {
+        FindObjectOfType<YakisobaGang.Player.Scripts.Player>().GetComponent<YakisobaGang.Player.Scripts.Player>()
+        .inventory[position].GetComponent<GenericGun>().EditFireRate(fireRate);
+    }
+
+}
