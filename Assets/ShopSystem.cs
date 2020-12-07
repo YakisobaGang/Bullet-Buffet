311b7da612996a688385d83f7917a61d07a9121d
Silvanodja <71275880+Silvanodja@users.noreply.github.com>
2020-12-07 10:56:06 -0300

    Commite

--- Assets/ShopSystem.cs
+++ Assets/ShopSystem.cs
@@ -4,37 +4,42 @@ using YakisobaGang.Player.Scripts;
 
 public class ShopSystem : MonoBehaviour
 {
-  public int _value;
-  public float upgrade;
-  public int position;
-  public int _actualValue;
-  private int _actualCash;
-  private Player _player;
-  public bool useCash;
+    public int _value;
+    public float upgrade;
+    public int position;
+    public int _actualValue;
+    private int _actualCash;
+    private Player _player;
+    public bool useCash;
 
-  private void Awake()
-  {
-    _value = _actualValue;
-    _player = GameObject.FindWithTag("Player").GetComponent<Player>();
-  }
-
-  private void LateUpdate()
-  {
-    ScoreAndCashSystem.Cash = _actualCash;
-  }
-
-  public void Buy()
-  {
-    if (_actualCash >= _actualValue)
+    private void Awake()
     {
-      ScoreAndCashSystem.Cash -= _value;
-      _actualValue += _value;
-      GunEdit(upgrade);
+        _value = _actualValue;
+        _player = GameObject.FindWithTag("Player").GetComponent<Player>();
     }
-  }
 
-  private void GunEdit(float fireRate)
-  {
-    _player.inventory[position].GetComponent<GenericGun>().EditFireRate(fireRate);
-  }
+    private void LateUpdate()
+    {
+        ScoreAndCashSystem.Cash = _actualCash;
+    }
+
+    public void Buy()
+    {
+        if (gameObject.GetComponentInParent<Transform>().name == "12Candy")
+        {
+            _player.inventory.Remove(_player.inventory[0]);
+            _player.inventory.Insert(0, _player.inventory[3]);
+        }
+        if (_actualCash >= _actualValue)
+        {
+            ScoreAndCashSystem.Cash -= _value;
+            _actualValue += _value;
+            GunEdit(upgrade);
+        }
+    }
+
+    private void GunEdit(float fireRate)
+    {
+        _player.inventory[position].GetComponent<GenericGun>().EditFireRate(fireRate);
+    }
 }
