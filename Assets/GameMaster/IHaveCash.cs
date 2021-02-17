6abeb16c1cbdeaa39947d62df0515025da93fbeb
Fabricio Duarte <fabriciohod@hotmail.com>
2021-02-17 16:29:38 -0300

    Arrumando sistema de dinheiro e bug no WaveSystem

---
+++ Assets/GameMaster/IHaveCash.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace GameMaster
+{
+    public interface IHaveScoreAndCash
+    {
+        public int Score { get; }
+        public int HighScore { get; }
+        public int Cash { get; }
+    }
+}
